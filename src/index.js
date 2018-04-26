00af6db9d2c24375bd818cf7eca85bfff6139734
jeffsantaines <javacodegod@gmail.com>
2018-04-26 19:11:50 +1200

    starter code

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react';
+import "./index.css";
+
+
+var destination = document.querySelector("#container");
+
+
+ReactDOM.render(
+    <div>
+        Hello
+    </div>, destination
+
+);
+
